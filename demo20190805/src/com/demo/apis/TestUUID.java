57d4360247275276bbe39c342123e93e9e996543
JasperZhao97 <zhaolei97@foxmail.com>
2019-08-05 14:46:17 +0800

    UUID类 通用唯一标识符的类

--- demo20190805/src/com/demo/apis/TestUUID.java
+++ demo20190805/src/com/demo/apis/TestUUID.java
@@ -1,9 +1,18 @@
 package com.demo.apis;
 
+import java.util.UUID;
+
 /**
  * @version 1.0.0
  * @auther ZhaoLei
  * @creatTime 2019年08月05日  14:39:07
  */
 public class TestUUID {
+
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        String uuidStr = uuid.toString();
+        System.out.println(uuidStr);
+        System.out.println(uuidStr.length());
+    }
 }
